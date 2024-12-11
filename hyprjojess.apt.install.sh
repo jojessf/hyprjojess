0dbd28d9814eb572ef9c475db7f2e9f177141dab
jojessf <jojessfournier@gmail.com>
2024-12-10 23:51:29 -0500

    add apt install, waybar power_menu.xml

---
+++ hyprjojess.apt.install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install hyprland wofi dolphin kitty waybar hyprland-backgrounds \
+hyprpaper hyprwayland-scanner fonts-font-awesome fonts-roboto \
+sway grim pavucontrol brightnessctl swaylock rofi slurp
