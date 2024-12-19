303fd5cf715ece1aafe9c15f6cbdfc74df8fc5cc
jojessf <jojessfournier@gmail.com>
2024-12-19 17:48:41 -0500

    more apt packages for mako-notifier, qt6-base-dev, cmake, wayland,hyprland-protocols, yada

--- hyprjojess.apt.install.sh
+++ hyprjojess.apt.install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt install hyprland wofi dolphin kitty waybar hyprland-backgrounds \
 hyprpaper hyprwayland-scanner fonts-font-awesome fonts-roboto \
-sway grim pavucontrol brightnessctl swaylock rofi slurp
+sway grim pavucontrol brightnessctl swaylock rofi slurp \
+hyprland-protocols hyprland-dev wayland-protocols \
+libwayland-client0 libsdbus-c++-bin libsdbus-c++-dev \
+cmake xdg-desktop-portal-gtk qt6-base-dev mako-notifier
